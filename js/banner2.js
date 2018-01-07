b3561c2c0bbfca5a96b4bfbac8915d7725d017cd
zodiacaca <121059464@qq.com>
2018-01-07 15:34:59 +0800

    regular update

--- js/banner2.js
+++ js/banner2.js
@@ -32,13 +32,13 @@
     // keyPoint3
     keyPoint.push({x:keyPoint[2].x+width/2*0.4, y:keyPoint[2].y-width/2*Math.sqrt(3)*0.4});
     // keyPoint4
-    keyPoint.push({x:width/2+width/2*0.1, y:width/2*Math.sqrt(3)});
+    keyPoint.push({x:width/2+width/2*0.12, y:width/2*Math.sqrt(3)});
     // keyPoint5
     keyPoint.push({x:width, y:keyPoint[4].y});
     // keyPoint6
     keyPoint.push({x:keyPoint[5].x-width/2*0.8, y:keyPoint[5].y-width/2*Math.sqrt(3)*0.8});
     // keyPoint7
-    keyPoint.push({x:keyPoint[6].x-width/2*0.5, y:keyPoint[6].y+width/2*0.5*Math.sqrt(3)});
+    keyPoint.push({x:keyPoint[6].x-width/2*0.52, y:keyPoint[6].y+width/2*0.52*Math.sqrt(3)});
     
     // path0
     path.push(document.createElementNS(svgNS,"path"));
@@ -88,20 +88,14 @@
   
   
   // construct widgets
-  var slider = document.getElementById("my_slider");
+/*   var slider = document.getElementById("my_slider");
   var output = document.getElementById("slider_output");
   output.innerHTML = slider.value; // Display the default slider value
   
   // Update the current slider value (each time you drag the slider handle)
   slider.oninput = function() {
     output.innerHTML = this.value;
-    // keyPoint2
-    keyPoint[2] = {x:keyPoint[1].x+width/2*this.value/100, y:keyPoint[1].y};
-    // keyPoint3
-    keyPoint[3] = {x:keyPoint[2].x+width/2*0.4, y:keyPoint[2].y-width/2*Math.sqrt(3)*0.4};
-    path[2].setAttributeNS(null,"d",
-      svgMove(keyPoint[2].x, keyPoint[2].y) + svgLine(keyPoint[3].x, keyPoint[3].y)
-    );
-  }
+    
+  } */
   
 }) ();
