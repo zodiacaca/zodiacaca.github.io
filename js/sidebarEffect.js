72d68e3172d6a9c8e33898e818e4e5fef1e3ffc8
zodiacaca <121059464@qq.com>
2018-07-12 12:17:47 +0800

    u

--- js/sidebarEffect.js
+++ js/sidebarEffect.js
@@ -1,8 +1,13 @@
 
-var SidebarMenuEffect = (function() {
+var SidebarMenuEffect = (function(){
   
   var bodyFrame = $("#main-frame");
   
-
 })();
+
+$(window).on("load", function(){
+  $("#nav-bar").css("transform", "translateX(-100%)")
+  $("#front-content").css("transform", "translateX(20%)")
+  $("#front-content").css("background-color", "#333")
+});
 
