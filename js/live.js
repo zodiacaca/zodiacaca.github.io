2567f9ef3ba23e0de6b8523351bc00f8c9b026dd
zodiacaca <121059464@qq.com>
2018-07-15 07:52:20 +0800

    u

--- js/live.js
+++ js/live.js
@@ -27,7 +27,7 @@
       pendingRequests = {},
       currentLinkElements = {},
       oldLinkElements = {},
-      interval = 100,
+      interval = 500,
       loaded = false,
       active = { "html": 1, "css": 1, "js": 1 };
 
