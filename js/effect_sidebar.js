74347a427f7ab70c2515260450701c895b2b9f54
zodiacaca <121059464@qq.com>
2018-07-18 13:24:31 +0800

    u

--- js/effect_sidebar.js
+++ js/effect_sidebar.js
@@ -9,9 +9,5 @@ function sceneOpen() {
   $("#nav-bar").css("transform", "translateX(-100%)");
   $("#board-slider").css("background-color", "#333");
   $("#board-slider").css("transform", "translateX(20%)");
-  $(".tag-item").css("box-shadow", "0 0 0 0.6rem #333");
-  setTimeout(function(){
-    $("#tag-list").css("background-image", "linear-gradient(to right, #f59393, #f59393)");
-  }, 800);
 }
 
