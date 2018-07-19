696353306805099097ce2f52894cca661226a4a9
zodiacaca <121059464@qq.com>
2018-07-19 15:16:32 +0800

    u

--- _site/js/effect_sidebar.js
+++ _site/js/effect_sidebar.js
@@ -1,4 +1,6 @@
 
+
+
 /* after document loaded */
 $(document).ready(function(){
   sceneOpen();
@@ -9,5 +11,17 @@ function sceneOpen() {
   $("#nav-bar")[0].style.transform = "translateX(-100%)";
   $("#board-slider")[0].style.backgroundColor = "#333";
   $("#board-slider")[0].style.transform = "translateX(20%)";
+  var left = getTagListLeftBorder();
+  setTimeout(function(){
+    $(".tag-item").each(function(){
+      this.style.backgroundPosition = left + "px 0";
+    });
+  }, 600);
 }
 
+function getTagListLeftBorder() {
+  
+  var left = $("#tag-list").offset().left;
+  
+  return left;
+}
