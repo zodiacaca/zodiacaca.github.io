e46f95fe384eab3529bb67d44eb9c58c1f544565
zodiacaca <121059464@qq.com>
2018-07-19 14:01:50 +0800

    u

--- _site/js/effect_sidebar.js
+++ _site/js/effect_sidebar.js
@@ -1,12 +1,15 @@
 
-$("#construct-helper-slider").slider({
+$("#helper-slider1").slider({
   range: true,
   min: 0,
   max: 100,
-  values: [0,50],
+  values: [0,100],
   slide: function(event, ui) {
-    $(".TTT")[0].style.left = ui.values[0] + "%";
-    $(".TTT")[0].innerHTML = ui.values[0];
+    $(".helper-TTT")[0].style.left = ui.values[0] + "%";
+    $(".helper-TTT")[0].innerHTML = ui.values[0];
+    // x position has been translated
+    $(".tag-item")[0].style.backgroundPosition = (ui.values[0] - 20) + "vw 0";
+    $(".tag-item")[1].style.backgroundPosition = (ui.values[0] - 20) + "vw 0";
   }
 });
 
