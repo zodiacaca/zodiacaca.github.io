1ee1439d741d98055453773c61c5f5ea8e80d59d
zodiacaca <121059464@qq.com>
2018-09-01 15:43:10 +0800

    u

--- _site/js/sidebar.js
+++ _site/js/sidebar.js
@@ -57,7 +57,7 @@
     $("#board-content").css("left", 0);
     // tags
     $("#tag-list").css("margin-right", 0);
-    $(".tag-item").css("background-position", -40 + "vw");
+    $(".tag-item").css("background-position", -35 + "vw");
   }
   
   /*
