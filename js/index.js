08e5afeffabc01598db25d539cd105fe0f2b784e
zodiacaca <121059464@qq.com>
2018-11-03 20:08:31 +0800

    u

--- js/index.js
+++ js/index.js
@@ -10,5 +10,14 @@
       $(this).addClass("menu-button--open");
     }
   });
+  $(".tag-button").click(function () {
+    if ($(".tags").hasClass("tags--open")) {
+      $(".tags").removeClass("tags--open");
+      $(".tag").removeClass("tag--open");
+    } else {
+      $(".tags").addClass("tags--open");
+      $(".tag").addClass("tag--open");
+    }
+  });
 } () );
 
